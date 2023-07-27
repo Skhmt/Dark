a761bff278cc9678b61be569ef5cc5b6592a1d51
Skhmt <Skhmt@users.noreply.github.com>
2023-07-27 15:32:45 -0700

    Using webview 0.7.6

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,23 @@
 
-// using aaronhuggins' 0.7.6 preview because webview 0.7.5 is broken with the latest Deno 1.35.x
-// this is NOT safe but all that can be done for now
-import {Webview, SizeHint} from 'https://raw.githubusercontent.com/aaronhuggins/webview_deno/0.7.6-preview2/mod.ts'
+import { SizeHint, Webview } from 'https://deno.land/x/webview@0.7.6/mod.ts'
 
 // outputting log to a file since this is supposed to act like a windows application without a console
 import * as log from 'https://deno.land/std@0.195.0/log/mod.ts'
 await log.setup({
-    handlers: {
-        file: new log.handlers.RotatingFileHandler("DEBUG", {
-            filename: "./log.txt",
-            formatter: "{levelName} {msg}", // {datetime} can be added here, but it's very verbose
-            maxBytes: 1024*1024,
-            maxBackupCount: 10
-        })
-    },
-    loggers: {
-        default: {
-            level: "DEBUG",
-            handlers: ["file"]
-        }
-    }
+	handlers: {
+		file: new log.handlers.RotatingFileHandler('DEBUG', {
+			filename: './log.txt',
+			formatter: '{levelName} {msg}', // {datetime} can be added here, but it's very verbose
+			maxBytes: 1024 * 1024,
+			maxBackupCount: 10,
+		}),
+	},
+	loggers: {
+		default: {
+			level: 'DEBUG',
+			handlers: ['file'],
+		},
+	},
 })
 
 // creating a webpage in a string - this can be read from a file instead
@@ -42,30 +40,32 @@ const html = `
 </html>
 `
 
-const webview = new Webview(true, // debug mode that allows console... currently this does nothing and is always true
-{
-    width: 620,
-    height: 450,
-    //can be .NONE, (dimensions are the default) .MIN (dimensions are the min), 
-    //.MAX (dimensions are the max), or .FIXED (cannot be changed by user)
-    hint: SizeHint.NONE
-})
-webview.title = "Hello Deno Webview"
+const webview = new Webview(
+	false, // debug mode that allows console... currently this does nothing and is always true
+	{
+		width: 620,
+		height: 450,
+		//can be .NONE, (dimensions are the default) .MIN (dimensions are the min),
+		//.MAX (dimensions are the max), or .FIXED (cannot be changed by user)
+		hint: SizeHint.NONE,
+	},
+)
+webview.title = 'Hello Deno Webview'
 
 let counter = 1
 webview.bind('press', (increment: number) => {
-    // return counter += increment
-    counter += increment || 1
+	// return counter += increment
+	counter += increment || 1
 
-    // automatically update the #output div
-    // this is not the ideal way to do this - the view should be updated in the frontend, not here...
-    // but this is just showing how it COULD be done.
-    webview.eval(`
-        document.querySelector('#output').innerHTML = ${counter};
+	// automatically update the #output div
+	// this is not the ideal way to do this - the view should be updated in the frontend, not here...
+	// but this is just showing how it COULD be done.
+	webview.eval(`
+		document.querySelector('#output').innerHTML = ${counter};
     `)
 
-    // async return this value to webviewland
-    return counter
+	// async return this value to webviewland
+	return counter
 })
 
 // this lets the webviewland log to denoland
